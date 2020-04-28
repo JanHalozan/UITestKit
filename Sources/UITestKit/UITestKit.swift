ff7b2db579b96b899253dbfaea63793fda344b86
Jan Halozan <j.halozan@gmail.com>
2020-04-28 18:13:45 +0200

    Update UITestKit.swift

--- Sources/UITestKit/UITestKit.swift
+++ Sources/UITestKit/UITestKit.swift
@@ -7,7 +7,7 @@ open class UITestKit {
     
     private let server: Server
     
-    init(port: SocketSwift.Port = 8080) {
+    public init(port: SocketSwift.Port = 8080) {
         self.server = Server()
         
         
